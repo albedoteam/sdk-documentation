205a450521631dcb45efb29c0edab6d6ea2b674f
fiduta <eryckson@gmail.com>
2021-02-06 12:39:31 -0300

    api versioning added

---
+++ src/Models/ApiDocumentVersion.cs
@@ -0,0 +1,23 @@
+﻿namespace AlbedoTeam.Sdk.Documentation.Models
+{
+    public readonly struct ApiDocumentVersion
+    {
+        public int MajorVersion { get; }
+        public int MinorVersion { get; }
+
+        public ApiDocumentVersion(int major = 1, int minor = 0)
+        {
+            MajorVersion = major;
+            MinorVersion = minor;
+        }
+
+        public override string ToString()
+        {
+            var version = $"v{MajorVersion}";
+            if (MinorVersion > 0)
+                version += $".{MinorVersion}";
+
+            return version;
+        }
+    }
+}
--- src/Setup.cs
+++ src/Setup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using AlbedoTeam.Sdk.Documentation.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.DependencyInjection;
+using NSwag;
 
 namespace AlbedoTeam.Sdk.Documentation
 {
@@ -9,22 +12,53 @@ namespace AlbedoTeam.Sdk.Documentation
     {
         public static IServiceCollection AddDocumentation(
             this IServiceCollection services,
-            Action<DocConfig> configureDocumentation)
+            Action<ApiDocument> configureDocumentation)
         {
             if (configureDocumentation == null)
                 throw new ArgumentNullException(nameof(configureDocumentation));
-            
-            var docConfig = new DocConfig();
-            configureDocumentation.Invoke(docConfig);
-            docConfig.DocumentName ??= "v1";
 
-            services.AddOpenApiDocument(d =>
+            var apiDocument = new ApiDocument();
+            configureDocumentation.Invoke(apiDocument);
+
+            foreach (var documentVersion in apiDocument.Versions)
+                services.AddOpenApiDocument(d =>
+                {
+                    var version = documentVersion.Key;
+
+                    d.Title = apiDocument.Title;
+                    d.Description = apiDocument.Description;
+                    d.DocumentName = version;
+                    d.ApiGroupNames = new[] {version};
+                    d.Version = version;
+
+                    d.PostProcess = document =>
+                    {
+                        document.Info.Contact = new OpenApiContact
+                        {
+                            Name = apiDocument.Contact.Name,
+                            Email = apiDocument.Contact.Email,
+                            Url = apiDocument.Contact.Url
+                        };
+                    };
+                });
+
+            services.AddApiVersioning(options =>
             {
-                d.Title = docConfig.Title;
-                d.Description = docConfig.Description;
-                d.DocumentName = docConfig.DocumentName;
-                d.ApiGroupNames = docConfig.GroupNames;
-                d.Version = docConfig.Version;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+
+                options.DefaultApiVersion = new ApiVersion(
+                    apiDocument.DefaultVersion.MajorVersion,
+                    apiDocument.DefaultVersion.MinorVersion);
+
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
             });
 
             return services;
